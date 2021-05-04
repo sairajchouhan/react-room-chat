cdd78b7979ef0e4f608708965e9ed3ab55d6ae3d
sairaj <sairaj2119@gmail.com>
2021-05-04 13:13:36 +0530

    add env variables for firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,9 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-import { firebaseConfig } from './utils/dontShow';
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+});
 
-const app = firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const db = firebase.firestore();
 export const timestamp = firebase.firestore.FieldValue.serverTimestamp;
