669000acfbdcf2af0533d7b81d1886862269f01e
sairaj <sairaj2119@gmail.com>
2021-05-08 22:01:49 +0530

    add delete and edit message ui

--- src/components/room/RoomMessage.tsx
+++ src/components/room/RoomMessage.tsx
@@ -1,6 +1,15 @@
-import { Box, Text } from '@chakra-ui/layout';
 import React from 'react';
+import { BiDotsHorizontalRounded } from 'react-icons/bi';
+import { Box, Divider, Text, VStack } from '@chakra-ui/layout';
 import firebase from 'firebase/app';
+import { Button, IconButton } from '@chakra-ui/button';
+import {
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+} from '@chakra-ui/popover';
 
 interface RoomMessageProps {
   isAuthUser: boolean;
@@ -37,20 +46,67 @@ const RoomMessageLayout: React.FC<{
       ml={isAuthUser ? 'auto' : 'inherit'}
       mr={isAuthUser ? 'inherit' : 'auto'}
       borderRadius="10px"
-      borderTopRightRadius={isAuthUser ? 0 : 'inherit'}
-      borderTopLeftRadius={isAuthUser ? 'inherit' : 0}
+      borderTopRightRadius={isAuthUser ? 0 : '10px'}
+      borderTopLeftRadius={isAuthUser ? '10px' : 0}
       my="1"
       maxW="75%"
+      className="message"
     >
-      <Text
-        fontWeight="medium"
-        fontStyle="italic"
-        fontSize="0.8rem"
-        textAlign={isAuthUser ? 'right' : 'left'}
-      >
-        {sender}
-      </Text>
-      <Text>{text}</Text>
+      <Box display="flex" alignItems="center" justifyContent="space-between">
+        {isAuthUser && (
+          <Box display="flex" alignItems="center" justifyContent="flex-start">
+            <Popover placement="top-start">
+              <PopoverTrigger>
+                <IconButton
+                  colorScheme="teal"
+                  aria-label="Send email"
+                  variant="link"
+                  visibility="hidden"
+                  sx={{
+                    '.message:hover &': {
+                      visibility: 'visible',
+                    },
+                  }}
+                  icon={<BiDotsHorizontalRounded size={20} />}
+                />
+              </PopoverTrigger>
+              <PopoverContent w="fit-content" p="0">
+                <PopoverArrow />
+                <PopoverBody padding="0">
+                  <VStack>
+                    <Button
+                      variant="ghost"
+                      width="100%"
+                      borderRadius="unset"
+                      color="red.400"
+                    >
+                      Delete Message
+                    </Button>
+                    <Divider p="0" m="0" />
+                    <Button
+                      width="100%"
+                      borderRadius="unset"
+                      variant="ghost"
+                      color="gray.600"
+                    >
+                      Edit Message
+                    </Button>
+                  </VStack>
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+          </Box>
+        )}
+        <Text
+          fontWeight="medium"
+          fontStyle="italic"
+          fontSize="0.8rem"
+          textAlign={isAuthUser ? 'right' : 'left'}
+        >
+          {sender}
+        </Text>
+      </Box>
+      <Text textAlign="right">{text}</Text>
     </Box>
   );
 };
